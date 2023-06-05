6164cb1d20d77bd051941959b2f3fe6d3b43fffa
shshmiao <852242576@qq.com>
2023-06-05 17:31:22 +0800

    解压

---
+++ 千锋2022版React全家桶教程_react零基础入门到项目实战完整版（资料）/基础部分代码资料/代码/material/005-JSX语法/src/index.js
@@ -0,0 +1,23 @@
+/*
+ * @作者: kerwin
+ * @公众号: 大前端私房菜
+ */
+// console.log("hello world kerwin")
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<div>
+    11111111
+</div>,document.getElementById("root"))
+
+
+// ReactDOM.render(React.createElement("div",{
+//     id:"aaa",
+//     class:"bbb"
+// },"111111111"),document.getElementById("root"))
+
+/*
+ jsx == js+xml
+*/
+
+
